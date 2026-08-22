87009b865573ca1a127e9bbe863f975d1f609599
nicosacristia-143 <nicosacristia@gmail.com>
2026-08-22 21:17:15 +0800

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,3 @@
+# THIS IS MY FIRST PROGRAMMING
+
+print("Hello, World!")
---
+++ activity3.py
@@ -0,0 +1,31 @@
+#using variables
+
+#int
+age = 18
+class_size = 49
+siblings = 4
+dogs = 1
+devices = 2
+
+#float
+speed = 102.3
+money = 20.99
+debt = -99999.1
+yearOfBirth =2008
+allowance = 100
+
+#string
+name = "Nicho"
+nickname = "niks or onics"
+gf = "jennie kim"
+school = "DLL College"
+fav_line = "kiss muna"
+
+#boolean
+isRich = True
+isHandsome = True
+isFilipino = True
+isSunny = False
+isRaining = True
+                                                    
+print("Hello! my name is " + name + " but you can call me " + nickname + " for short, I'm " + str(age) + " years old with " + str(siblings) + " siblings. Im studying in " + school + " with daily allowance of " + str(allowance) + " I love my gf " + gf)
