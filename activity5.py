9d2087df54eeb8662024e283141ca43831b4211f
nicosacristia-143 <nicosacristia@gmail.com>
2026-08-30 20:02:46 +0800

    Update activity5.py

--- activity5.py
+++ activity5.py
@@ -7,14 +7,14 @@ sum = a + b
 diff = a - b 
 product = a * b 
 quotient = a / b 
-expo = a ** b 
-fd = a // b 
+exponent = a ** b 
+floor_div = a // b 
 modulos = a % b 
 
 print("the sum of", a, "and", b, "is", sum)
 print("the difference of", a, "and", b, "is", diff)
 print("the product of", a, "and", b, "is", product)
 print("the quotient of", a, "and", b, "is", quotient)
-print("the exponential value of", a, "and", b, "is", expo)
-print("the floor division of", a, "and", b, "is", fd)
-print("the modulo/s of", a, "and", b, "is", modulos)
+print("the exponential value of", a, "and", b, "is", exponent)
+print("the floor division of", a, "and", b, "is", floor_div)
+print("the modulo of", a, "and", b, "is", modulos)
