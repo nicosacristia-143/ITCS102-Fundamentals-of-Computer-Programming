ee71c1f681c5d3c41ab35e6a012eaa4492a1d3c3
nicosacristia-143 <nicosacristia@gmail.com>
2026-08-30 19:55:09 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,20 @@
+#arithmetic operators
+
+a = 6
+b = 3
+
+sum = a + b 
+diff = a - b 
+product = a * b 
+quotient = a / b 
+expo = a ** b 
+fd = a // b 
+modulos = a % b 
+
+print("the sum of", a, "and", b, "is", sum)
+print("the difference of", a, "and", b, "is", diff)
+print("the product of", a, "and", b, "is", product)
+print("the quotient of", a, "and", b, "is", quotient)
+print("the exponential value of", a, "and", b, "is", expo)
+print("the floor division of", a, "and", b, "is", fd)
+print("the modulo/s of", a, "and", b, "is", modulos)
