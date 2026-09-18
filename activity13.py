85593a63a2cee7d374b04623918c39c3bbb49a77
nicosacristia-143 <nicosacristia@gmail.com>
2026-09-18 14:54:06 +0800

    Update activity13.py

--- activity13.py
+++ activity13.py
@@ -1,10 +1,10 @@
 #pogi ako
 
 age = int(input("Enter your age: "))
-is_employed = bool(input("Are you currently employed? [True/False]"))
+is_employed = bool(input("Are you currently employed? [True/False]:"))
 credit_score = int(input("Enter your credit score: "))
 annual_income = float(input("Enter your annual income: "))
-has_collateral = bool(input("Do you have collateral?"))
+has_collateral = bool(input("Do you have collateral?: "))
 
 base_interest_rate = 5.0
 if age >= 21 and is_employed == True :
@@ -12,13 +12,21 @@ if age >= 21 and is_employed == True :
         base_interest_rate = 5.0
         if annual_income >= 100000:
            base_interest_rate = 4.5
+        else: 
+            print("You are eligible for a loan with a base interest rate of 5.0%.")
+
         print("You are eligible for a loan with a base interest rate of 4.5%.")
     elif 600 <= credit_score < 750:
         base_interest_rate = 8.0
-        if annual_income >= 100000:
-            print("You are eligible for a loan with a base interest rate of 8.0%.")
-            base_interest_rate = 6.5
+        if has_collateral == True:
+            base_interest_rate = 7.0
+            print("You are eligible for a loan with a base interest rate of 7.0%.")
+        elif annual_income < 40000:
+            base_interest_rate = 9.5
+            print("You are eligible for a loan with a base interest rate of 9.5%.")
+        elif credit_score < 600:
+            print("Rejected: Credit score too low")
     else:
-        base_interest_rate = 0.10
+        print("Rejected: Credit score too low")
 else:
     print("You are not eligible for a loan due to age or employment status.")
